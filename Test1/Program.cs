30d5e7f49ad054541ae289b18cb41ca0df7676c6
savemysoul382 <serjke88@gmail.com>
2018-02-10 20:12:27 +0300

    Добавьте файлы проекта.

---
+++ Test1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int num, num_1;
+            
+            Console.WriteLine("Enter first number: ");
+            num = Convert.ToInt32() (Console.ReadLine());
+            Console.WriteLine("\nEnter second number: ");
+            num_1 = Convert.ToInt32() (Console.ReadLine());
+            Console.WriteLine("The result is: " + (num + num_1));
+        }
+    }
+}
