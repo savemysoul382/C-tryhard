08e685a33f6788b69b421d08073ba042e75c59ef
savemysoul382 <serjke88@gmail.com>
2018-02-10 22:02:36 +0300

    -Realization of checking the age (only > than 18.23) -trying "const" and "double"

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -10,14 +10,28 @@ namespace Test1
     {
         static void Main (string[] args)
         {
-            int num, num_1;
-            
-            Console.WriteLine("Enter first number: ");
-            num = Convert.ToInt32 (Console.ReadLine());  //Конвертация в число.
-            Console.WriteLine("\nEnter second number: ");
-            num_1 = Convert.ToInt32 (Console.ReadLine());  //Конвертация в число.
-            Console.WriteLine("The result is: " + (num + num_1).ToString());
+            const float p = 3.14f;  // trying const
+            double num;
+
+            Console.WriteLine("How old are you?");
+            num = Convert.ToDouble(Console.ReadLine());  // trying double
+
+            if (num > 18.23d)
+            {
+                Console.WriteLine("You are older than 18.23");
+            }
+
             Console.ReadKey();
+
+            //Calculator practice
+//            int num, num_1;
+//            
+//            Console.WriteLine("Enter first number: ");
+//            num = Convert.ToInt32 (Console.ReadLine());  //Конвертация в число.
+//            Console.WriteLine("\nEnter second number: ");
+//            num_1 = Convert.ToInt32 (Console.ReadLine());  //Конвертация в число.
+//            Console.WriteLine("The result is: " + (num + num_1).ToString());
+//            Console.ReadKey();
         }
     }
 }
